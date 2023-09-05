1f723b9021cbffe054c34d06e28efdfeb38c573c
R4U1R4M1R3Z <113516449+R4U1R4M1R3Z@users.noreply.github.com>
2023-09-05 13:35:01 +0200

    Respeta formato del documento

--- automa.py
+++ automa.py
@@ -1,39 +1,45 @@
 import os
 import pandas as pd
-from docx import Document
+from docxtpl import DocxTemplate
 
 # Cargar el archivo Excel
-excel_file = "XXX.xlsx"
+excel_file = "D:\\script\\data.xlsx"
 df = pd.read_excel(excel_file)
 
 # Ruta de la plantilla de Word y carpeta de salida para los documentos generados
-template_path = "XXX.docx"
-output_folder = "XXX/XXXX"
+template_path = "D:\\script\\template.docx"
+output_folder = "D:\\script\\documentos"
 
 # Iterar a través de los registros en el DataFrame
 for index, row in df.iterrows():
-    row1 = row["XXX"]
-    row2 = row["xxx"]
-    row3 = row["xxx"]
-    
-    # Cargar la plantilla de Word
-    doc = Document(template_path)
-    
-    # Buscar y reemplazar el marcador con el número de bastidor en párrafos
-    for paragraph in doc.paragraphs:
-        if "variable" in paragraph.text:
-            paragraph.text = paragraph.text.replace("variable", row1)
-    
-    # Buscar y reemplazar en tablas
-    for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                if "variable" in cell.text:
-                    cell.text = cell.text.replace("variable", row1)
+    furgon = row["FURGON"]
+    chasis = row["CHASIS"]
+    equipo_frio = row["EQUIPO_FRIO"]
+    NUM_SERIE = row["NUM_SERIE"]
+
+    # Cargar la plantilla de Word como una plantilla docxtpl
+    doc = DocxTemplate(template_path)
+
+    # Definir los datos a reemplazar en la plantilla
+    context = {
+        'FURGON': furgon,
+        'CHASIS': chasis,
+        'EQUIPO_FRIO': equipo_frio,
+        'NUM_SERIE': NUM_SERIE
+    }
+
+    # Renderizar la plantilla con los datos
+    doc.render(context)
     
     # Guardar el documento Word generado
-    output_filename = f"{output_folder}\\XXX {row1}.docx"
+    output_filename = f"{output_folder}\\Certificado - {chasis}.docx"
     doc.save(output_filename)
+
+print("Documentos generados correctamente.")
+
+# Abrir la carpeta con los archivos generados
+os.system(f'explorer "{output_folder}"')
+
 
 print("Documentos generados correctamente.")
 
